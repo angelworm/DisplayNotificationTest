6c3fcbf88cd2ff7cbb8a4fa77af504e3f6b5d6c8
NAKAJIMA Susumu <ueda2008+git@gmail.com>
2013-10-30 15:16:37 +0900

    initial commit

--- DisplayNotificationTest/AGAppDelegate.m
+++ DisplayNotificationTest/AGAppDelegate.m
@@ -8,11 +8,33 @@
 
 #import "AGAppDelegate.h"
 
+#import <ApplicationServices/ApplicationServices.h>
+
+
+void MyDisplayReconfigurationCallBack (
+                                       CGDirectDisplayID display,
+                                       CGDisplayChangeSummaryFlags flags,
+                                       void *userInfo)
+{
+    CGDisplayChangeSummaryFlags capture = kCGDisplayDesktopShapeChangedFlag;
+        //kCGDisplayAddFlag | kCGDisplayRemoveFlag;
+    if (flags & capture && CGMainDisplayID() == display ) {
+        NSLog(@"desktop shape has changed");
+        NSUserNotification *not = [[NSUserNotification alloc] init];
+        not.title = @"DisplayNotificationTest";
+        not.informativeText = @"desktop shape has changed";
+        
+        [[NSUserNotificationCenter defaultUserNotificationCenter]
+         deliverNotification:not];
+    }
+}
+
 @implementation AGAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    CGDisplayRegisterReconfigurationCallback(MyDisplayReconfigurationCallBack, NULL);
 }
 
 @end
